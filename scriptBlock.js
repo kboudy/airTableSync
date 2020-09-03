c50b539e953f1eadc7e491b6f471872df67ac7cc
Keith Boudreau <kboudy@gmail.com>
2020-09-03 11:19:18 -0400

    backing up the airtable script block here

--- scriptBlock.js
+++ scriptBlock.js
@@ -1,29 +1,28 @@
-// this will live on airtable
-
 const API_BASE_URL = "https://api.airtable.com/v0/";
+const UNIQUE_FIELD = "Name";
+const DATE_FORMAT = "ddd MMM D YYYY h:mm A ZZ";
 
-const setLastSyncDate = async () => {
+const setLastSyncDate = async (timestamp) => {
   const syncTable = base.getTable("SyncInfo");
   const query = await syncTable.selectRecordsAsync();
   await syncTable.updateRecordAsync(query.records[0], {
-    LastSyncDate: new Date(),
+    LastSyncTimestamp: timestamp,
   });
 };
 
 const getSyncInfo = async () => {
   const syncTable = base.getTable("SyncInfo");
   const query = await syncTable.selectRecordsAsync();
+  const record = query.records[0];
   const syncInfo = {
-    lastSyncDate: query.records[0].getCellValue("LastSyncDate"),
-    apiKey: query.records[0].getCellValue("ApiKey"),
-    destinationBaseId: query.records[0].getCellValue("DestinationBaseId"),
+    lastSyncTimestamp: record.getCellValue("LastSyncTimestamp"),
+    apiKey: record.getCellValue("ApiKey"),
+    destinationBaseId: record.getCellValue("DestinationBaseId"),
   };
   return syncInfo;
 };
 
-const syncInfo = await getSyncInfo();
-
-const getRecords = async (tableName, fields, filterFormula) => {
+const getRecords = async (baseId, tableName, fields, filterFormula) => {
   const allRecords = [];
   let offset;
   do {
@@ -37,7 +36,7 @@ const getRecords = async (tableName, fields, filterFormula) => {
     if (offset) {
       params.offset = offset;
     }
-    let queryUrl = `${API_BASE_URL}${syncInfo.destinationBaseId}/${tableName}?`;
+    let queryUrl = `${API_BASE_URL}${baseId}/${tableName}?`;
     for (const k of Object.keys(params)) {
       queryUrl = `${queryUrl}${k}=${encodeURIComponent(params[k])}&`;
     }
@@ -55,6 +54,9 @@ const getRecords = async (tableName, fields, filterFormula) => {
       headers: { Authorization: `Bearer ${syncInfo.apiKey}` },
     });
     const json = await response.json();
+    if (json.error) {
+      throw new Error(json.error);
+    }
     allRecords.push(...json.records);
     offset = json.offset;
   } while (offset);
@@ -62,36 +64,120 @@ const getRecords = async (tableName, fields, filterFormula) => {
   return allRecords;
 };
 
-const createRecords = async (tableName, records) => {
+const createRecords = async (baseId, tableName, records) => {
   // NOTE: airtable REST API for "create" method allows a max of 10 records per request
   let remainingRecords = [...records];
 
   while (remainingRecords.length > 0) {
     const currentChunk = remainingRecords.slice(0, 10);
     remainingRecords = remainingRecords.slice(10);
-    const response = await fetch(
-      `${API_BASE_URL}${syncInfo.destinationBaseId}/${tableName}?`,
-      {
-        headers: {
-          Authorization: `Bearer ${syncInfo.apiKey}`,
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-        body: JSON.stringify({ records: currentChunk }),
-      }
-    );
+    const response = await fetch(`${API_BASE_URL}${baseId}/${tableName}?`, {
+      headers: {
+        Authorization: `Bearer ${syncInfo.apiKey}`,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ records: currentChunk }),
+    });
+    const json = await response.json();
+    if (json.error) {
+      throw new Error(json.error);
+    }
   }
 };
 
-//const results = await getRecords("Fruits",["Name","Color"]);
+const updateRecords = async (baseId, tableName, records) => {
+  // NOTE: airtable REST API for "update" method allows a max of 10 records per request
+  let remainingRecords = [...records];
 
-const records = [];
-for (let i = 1; i <= 25; i++) {
-  records.push({
-    fields: {
-      Name: `n${i}`,
-      Color: `c${i}`,
-    },
-  });
+  while (remainingRecords.length > 0) {
+    const currentChunk = remainingRecords.slice(0, 10);
+    remainingRecords = remainingRecords.slice(10);
+    const response = await fetch(`${API_BASE_URL}${baseId}/${tableName}?`, {
+      headers: {
+        Authorization: `Bearer ${syncInfo.apiKey}`,
+        "Content-Type": "application/json",
+      },
+      method: "PATCH",
+      body: JSON.stringify({ records: currentChunk }),
+    });
+    const json = await response.json();
+    if (json.error) {
+      throw new Error(json.error);
+    }
+  }
+};
+
+const deleteRecords = async (baseId, tableName, ids) => {
+  // NOTE: airtable REST API for "delete" method allows a max of 10 records per request
+  let remainingIds = [...ids];
+
+  while (remainingIds.length > 0) {
+    const currentChunkIds = remainingIds.slice(0, 10);
+    remainingIds = remainingIds.slice(10);
+    let queryUrl = `${API_BASE_URL}${baseId}/${tableName}?`;
+    for (const id of currentChunkIds) {
+      queryUrl = `${queryUrl}&${encodeURIComponent("records[]")}=${id}&`;
+    }
+    queryUrl = queryUrl.slice(0, queryUrl.length - 1);
+    const response = await fetch(queryUrl, {
+      headers: { Authorization: `Bearer ${syncInfo.apiKey}` },
+      method: "DELETE",
+    });
+    const json = await response.json();
+    if (json.error) {
+      throw new Error(json.error);
+    }
+  }
+};
+
+const tableToSync = "Fruits";
+const jobTimestamp = new Date().getTime();
+const syncInfo = await getSyncInfo();
+
+const sourceKeys = (await getRecords(base.id, tableToSync, [UNIQUE_FIELD])).map(
+  (r) => r.fields[UNIQUE_FIELD]
+);
+const destinationKeysAndIds = (
+  await getRecords(syncInfo.destinationBaseId, tableToSync, [UNIQUE_FIELD])
+).reduce((acc, r) => {
+  acc[r.fields[UNIQUE_FIELD]] = r.id;
+  return acc;
+}, {});
+const destinationKeys = Object.keys(destinationKeysAndIds);
+
+const keysToAdd = sourceKeys.filter((k) => !destinationKeys.includes(k));
+const keysToDelete = destinationKeys.filter((k) => !sourceKeys.includes(k));
+
+const filterFormula = syncInfo.lastSyncTimestamp
+  ? "DATETIME_DIFF(LAST_MODIFIED_TIME(), " +
+    `DATETIME_PARSE(${syncInfo.lastSyncTimestamp}, 'x'),` +
+    "'seconds') > 0"
+  : null;
+const changedSourceRecords = await getRecords(
+  base.id,
+  tableToSync,
+  null,
+  filterFormula
+);
+const recordsToAdd = [];
+const recordsToUpdate = [];
+
+for (const r of changedSourceRecords) {
+  const recordKey = r.fields[UNIQUE_FIELD];
+  if (keysToAdd.includes(recordKey)) {
+    recordsToAdd.push({ fields: r.fields });
+  } else {
+    recordsToUpdate.push({
+      id: destinationKeysAndIds[recordKey],
+      fields: r.fields,
+    });
+  }
 }
-await createRecords("Fruits", records);
+await createRecords(syncInfo.destinationBaseId, tableToSync, recordsToAdd);
+
+await updateRecords(syncInfo.destinationBaseId, tableToSync, recordsToUpdate);
+
+const idsToDelete = keysToDelete.map((k) => destinationKeysAndIds[k]);
+await deleteRecords(syncInfo.destinationBaseId, tableToSync, idsToDelete);
+await setLastSyncDate(jobTimestamp);
